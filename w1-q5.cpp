3dacce9d2784f3210ec1c5255fcc085c9ec7e09d
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-07-27 00:53:51 +0530

    Week 1 question 5 string concatenation

--- w1-q5.cpp
+++ w1-q5.cpp
@@ -4,15 +4,16 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-  string s1 = "Welcome ";
-  string s2 = "students";
-  char x = 'x';
+int main(int argc, char const *argv[])
+{
+    string s1 = "Welcome ";
+    string s2 = "students";
+    char x = 'x';
 
-  // s1 += s2;
+    // s1 += s2;
 
-  // s1.append(s2);
+    // s1.append(s2);
 
-  cout << "Message: " << s1;
-  return 0;
+    cout << "Message: " << s1;
+    return 0;
 }
